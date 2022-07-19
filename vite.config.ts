899f87b3d4c05b7142aff59fc7813a56649632bf
Kirill Kumma <kirill.kumma@gmail.com>
2022-07-19 12:57:24 +0300

    init

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { readdirSync } from 'fs'
+import { resolve } from 'path'
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const topLevelDirs = readdirSync(__dirname, { withFileTypes: true }).reduce(
+  (acc: Record<string, string>, file) => {
+    if (file.isDirectory() && file.name !== 'node_modules') {
+      acc[file.name] = resolve(__dirname, file.name)
+    }
+    return acc
+  },
+  {}
+)
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: topLevelDirs,
+  },
+})
